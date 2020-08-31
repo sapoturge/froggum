4824284932eced9f3b932fe10530d6b20fda80c3
John Hiatt <jhia186034@adams12.org>
2020-08-31 15:59:59 -0600

    Add debug print statement to post_install.py
    This is just to see if it runs in the automatic build log.

--- meson/post_install.py
+++ meson/post_install.py
@@ -6,4 +6,5 @@ import subprocess
 schemadir = os.path.join(os.environ["MESON_INSTALL_PREFIX"], "share", "glib-2.0", "schemas")
 
 if not os.environ.get("DESTDIR"):
+    print("Compiling schema...")
     subprocess.call(["glib-compile-schemas", schemadir])
