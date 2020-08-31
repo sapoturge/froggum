99355c7c47b7f3ab116c84463bb5ab5c17398e94
John Hiatt <jhia186034@adams12.org>
2020-08-31 15:45:10 -0600

    Add post_install.py

---
+++ meson/post_install.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+import os
+import subprocess
+
+schemadir = os.path.join(os.environ["MESON_INSTALL_PREFIX"], "share", "glib-2.0", "schemas")
+
+if not os.environ.get("DESTDIR"):
+    subprocess.call(["glib-compile-schemas", schemadir])
